4b803a7f4b9c1ca38be43773e85a2450752c3c31
Said Abdulsalam <sabdulsalam@ubtconsults.se>
2024-10-05 06:56:41 +0100

    fix: update

--- database/migrations/1_create_memos_table.php
+++ database/migrations/1_create_memos_table.php
@@ -14,6 +14,7 @@ return new class extends Migration
             $table->string('owner_type');
             $table->string('title');
             $table->tinyInteger('type')->default(0);
+            $table->text('files'); 
             $table->text('content');
             $table->timestamps();
             $table->tinyInteger('status')->default(0);
